87f0121fddebd8f64c33a5e407539c23ecef5066
austin <austinchang4@gmail.com>
2021-11-09 14:09:23 +0800

    increment site score

--- processor/process.go
+++ processor/process.go
@@ -1,6 +1,9 @@
 package processor
 
-import "quickscrape/extractor"
+import (
+	"net/url"
+	"quickscrape/extractor"
+)
 
 func ProcessPostResults(r *extractor.Results) error {
 	// increment site score for all external links
@@ -10,12 +13,32 @@ func ProcessPostResults(r *extractor.Results) error {
 	for _, l := range r.RelatedExternalLinks {
 		if mapLinks[l] == nil {
 			*links = append(*links, l)
-			mapLinks[l] = nil
+			mapLinks[l] = struct{}{}
 		}
 	}
+	mapLinks = nil // gc
 	// - each external link
 	// -- externLink.site.score += r.site.score * 0.1 // max cap = 10
 	// if already exist, overwrite token scores
 	// insert record into to db
+
+	originSiteScore := new(float32)
+	if err := getSiteScore(&r.Site, originSiteScore); err != nil {
+		return err
+	}
+
+	for _, l := range *links {
+		// get site
+		u, err := url.Parse(l)
+		if err != nil {
+			return err
+		}
+		// get site score
+		score := new(float32)
+		if err := getSiteScore(&u.Host, score); err != nil {
+			return err
+		}
+		*score += *originSiteScore
+	}
 	return nil
 }
--- processor/site.go
+++ processor/site.go
@@ -1,1 +1,22 @@
 package processor
+
+import (
+	"context"
+	"quickscrape/db"
+
+	"github.com/georgysavva/scany/pgxscan"
+)
+
+func getSiteScore(site *string, score *float32) error {
+	conn, err := db.PG.Acquire(context.Background())
+	if err != nil {
+		return err
+	}
+	defer conn.Release()
+	if err := pgxscan.Get(context.Background(), conn, score, `
+		SELECT score FROM site WHERE site = $1
+	`, *site); err != nil {
+		return err
+	}
+	return nil
+}
