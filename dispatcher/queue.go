acc551c1436a17039ed51c14feb6ded387952d27
austin <austinchang4@gmail.com>
2021-11-16 23:24:14 +0800

    retry scrape

--- dispatcher/queue.go
+++ dispatcher/queue.go
@@ -7,27 +7,43 @@ import (
 	"time"
 )
 
+const SCRAPE_MAX_RETRY = 3
+
 var queue []string = make([]string, 0)
 
 func queueProcessor() {
-	for i, url := range queue {
+	queueLen := len(queue)
+	log.Println("Queue length:", queueLen) // debug
+	for _, url := range queue {
 		// scrape url
-		queue = queue[i+1:]
 		go func(url string) error {
 			if checkLinkExist(url) {
 				return nil
 			}
+			log.Printf("Scraping %s", url) // debug
 			ext := new(extractor.Extractor)
 			results := new(extractor.Results)
-			if err := ext.ExtractLink(url, results); err != nil {
-				log.Println(err.Error())
-				return err
+			{
+				// retry scraping 3 times
+				for i := 0; i < SCRAPE_MAX_RETRY; i++ {
+					if err := ext.ExtractLink(url, results); err != nil {
+						log.Println(err.Error())
+						if i == SCRAPE_MAX_RETRY-1 {
+							return err
+						}
+						log.Printf("Failed to scrape %s, retrying ...", url) // debug
+						continue
+					}
+					break
+				}
 			}
 
+			log.Printf("Sending %s to indexer", url) // debug
 			if err := processor.ProcessPostResults(results); err != nil {
 				return err
 			}
 
+			log.Printf("Extract %d external links + %d internal links from  %s", len(results.RelatedExternalLinks), len(results.RelatedInternalLinks), url) // debug
 			queue = append(queue, results.RelatedExternalLinks...)
 			queue = append(queue, results.RelatedInternalLinks...)
 
@@ -35,6 +51,7 @@ func queueProcessor() {
 
 		}(url)
 	}
+	queue = queue[queueLen:]
 }
 
 func processQueue() {
