a0a661dafb03c6589bebdd58df5504f7b5e0fb86
austin <austinchang4@gmail.com>
2021-11-24 02:11:04 +0800

    check by url instead of id

--- dispatcher/check.go
+++ dispatcher/check.go
@@ -2,18 +2,12 @@ package dispatcher
 
 import (
 	"context"
-	"crypto/sha512"
-	"fmt"
 	"quickscrape/db"
 
 	"github.com/georgysavva/scany/pgxscan"
 )
 
 const TRACK_SITE_SCORE = 200
-
-func genPostID(url string) string {
-	return fmt.Sprintf("%x", sha512.Sum512([]byte(url)))
-}
 
 func checkLinkExist(url string) bool {
 	conn, err := db.PG.Acquire(context.Background())
@@ -27,10 +21,10 @@ func checkLinkExist(url string) bool {
 		SELECT posts.url from posts
         LEFT JOIN sites
         ON posts.site = sites.site
-        WHERE id  = $1
+        WHERE url  = $1
         AND sites.score < $2
         AND posts.updated_at > current_date - interval '7 day'
-    `, genPostID(url), TRACK_SITE_SCORE); err != nil {
+    `, url, TRACK_SITE_SCORE); err != nil {
 		return false
 	}
 	if len(*t) == 0 {
